d8d5287a1d55b9e5f8d70f5c34577376d1650469
Javier Esbri Gamon <1dawnm161725@gmail.com>
2016-12-02 11:52:44 +0000

    Initial Status of the Project

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+import org.japo.java.app.App;
+
+/**
+ *
+ * @author CicloM
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Crar aplicación
+        App app = new App();
+        
+        //Lanzar aplicación
+        app.lanzarAplicacion();
+    }
+
+}
